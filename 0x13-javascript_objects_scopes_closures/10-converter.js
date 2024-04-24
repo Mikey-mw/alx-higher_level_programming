d9b29727b08f44e0ef01f7086ef5029b5d47b53c
Mikey-mw <mikeywubshet@gmail.com>
2024-04-24 02:11:12 -0700

    try 4

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-let count = 0;
-exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
 
